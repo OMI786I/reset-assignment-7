f58ee46d7833fe9835ffd2aba79f120236082c5a
OMI786I <sohrawardy1998@gmail.com>
2024-05-11 22:35:10 +0600

    added banner section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import "./App.css";
+import Banner from "./components/banner/banner";
 import Header from "./components/header/Header";
 
 function App() {
   return (
     <>
       <Header></Header>
+
+      <Banner></Banner>
     </>
   );
 }
---
+++ src/components/banner/banner.jsx
@@ -0,0 +1,32 @@
+const Banner = () => {
+  return (
+    <div>
+      <div
+        className="hero min-h-screen"
+        style={{
+          backgroundImage: "url(https://i.ibb.co/zN1nHzP/Rectangle-1-1.png)",
+        }}
+      >
+        <div className=" "></div>
+        <div className="hero-content text-center text-neutral-content">
+          <div className="max-w-screen-md">
+            <h1 className="mb-5 text-5xl font-bold">
+              Discover an exceptional cooking class tailored for you!
+            </h1>
+            <p className="mb-5">
+              Learn and Master Basic Programming, Data Structures, Algorithm,
+              OOP, Database and solve 500+ coding problems to become an
+              exceptionally well world-class Programmer
+            </p>
+            <button className="btn btn-accent mr-7">Explore Now</button>
+            <button className="btn  btn-ghost border-white ">
+              Our feedback
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
